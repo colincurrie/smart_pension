91f3def5239077c20cde6fd8ebfc0aae28496271
Colin Currie <colin@myroadtowembley.com>
2022-10-31 15:42:36 +0000

    Populating the specs and added a Gemfile
    because Im using pry

--- parser.rb
+++ parser.rb
@@ -2,13 +2,18 @@ class Parser
 
   def initialize(log)
     # It must take a log file as an argument
+    unless log && File.exists?(log)
+      raise ArgumentError('You must specify a valid logfile')
+    end
   end
 
   def most_page_views
     # tally up each line by the first argument
+    []
   end
 
   def most_unique_pages
     # not sure what is meant here, unique page and IP combination?
+    []
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,26 +1,50 @@
+require 'pry'
+
+require_relative '../parser.rb'
+
 describe Parser do
 
+  let(:log)     { 'webserver.log' }
+  let(:subject) { Parser.new(log) }
+
   it 'receives a log as an argument' do 
-    # expect Parser.new(logfile) not to raise an error
+    expect { Parser.new(log) }.not_to raise_error
+  end
+
+  it 'fails if no log is provided' do 
+    expect { Parser.new }.to raise_error(ArgumentError)
   end
 
   describe '#most_page_views' do
+
+    let(:subject) { Parser.new(log).most_page_views }
+
     it 'puts the most popular page first' do
       # expect the first result to have the highest count
+      counts = subject.map(&:last)
+      expect(counts.first).to eq(counts.max)
     end
 
     it 'puts the least popular page last' do
-      # expect the last reqult to have the smallest count
+      # expect the last result to have the smallest count
+      counts = subject.map(&:last)
+      expect(counts.last).to eq(counts.min)
     end
   end
 
   describe '#most_unique_pages' do
+
+    let(:subject) { Parser.new(log).most_unique_pages }
+    let(:counts)  { subject.map(&:last) }
+
     it 'puts the most unique page first' do
       # expect the most unique page first
+      expect(counts.first).to eq(counts.min)
     end
 
     it 'should put the least unique page last' do
       # expect the least unique page last
+      expect(counts.first).to eq(counts.max)
     end
   end
 end
