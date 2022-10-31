dafb5dc45369b761bf2049635953c8b7953f50a2
Colin Currie <colin@myroadtowembley.com>
2022-10-31 15:13:25 +0000

    Adding an RSpec test for the parser

---
+++ spec/parser_spec.rb
@@ -0,0 +1,26 @@
+describe Parser do
+
+  it 'receives a log as an argument' do 
+    # expect Parser.new(logfile) not to raise an error
+  end
+
+  describe '#most_page_views' do
+    it 'puts the most popular page first' do
+      # expect the first result to have the highest count
+    end
+
+    it 'puts the least popular page last' do
+      # expect the last reqult to have the smallest count
+    end
+  end
+
+  describe '#most_unique_pages' do
+    it 'puts the most unique page first' do
+      # expect the most unique page first
+    end
+
+    it 'should put the least unique page last' do
+      # expect the least unique page last
+    end
+  end
+end
