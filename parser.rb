84ea93301f0895e5b387ee7fa3017d77ece5fe88
Colin Currie <colin@myroadtowembley.com>
2022-10-31 15:20:44 +0000

    Adding the parser

---
+++ parser.rb
@@ -0,0 +1,14 @@
+class Parser
+
+  def initialize(log)
+    # It must take a log file as an argument
+  end
+
+  def most_page_views
+    # tally up each line by the first argument
+  end
+
+  def most_unique_pages
+    # not sure what is meant here, unique page and IP combination?
+  end
+end
